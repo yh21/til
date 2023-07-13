251b35cd1200a6402969a560ec1e58fe2868edac
yh_21 <yh_21@naver.com>
2023-07-13 21:23:27 +0900

    init

---
+++ python_300/011-020.py
@@ -0,0 +1,35 @@
+삼성전자 = 50000
+print(삼성전자 * 10) #011
+
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+print(type(PER)) #012
+
+s = "hello"
+t = "python"
+print(s+"!", t) #013
+
+print(2 + 2 * 3) #014
+
+a = "132"
+print(type(a)) #015
+
+num_str = "720"
+x = int(num_str)
+print(type(x)) #016
+
+num = 100
+y = str(num)
+print(type(y)) #017
+
+data = "15.79"
+z = float(data)
+print(type(z)) #018
+
+year = "2020"
+year_num = int(year)
+print(year_num, year_num - 1, year_num - 2) #019
+
+cost = 48584
+print(cost * 36) #020
