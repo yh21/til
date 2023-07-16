d161ee9e3ccd1ebc06ab11fea966705cf3f2e27e
yh_21 <yh_21@naver.com>
2023-07-16 21:16:21 +0900

    message

--- python_300/041-050.py
+++ python_300/041-050.py
@@ -14,4 +14,18 @@ print(file_name.endswith("xlsx")) #044
 print(file_name.endswith(("xlsx", "xls"))) #045
 
 file_name_1 = "2020_보고서.xlsx"
-print(file_name_1.startswith("2020"))
+print(file_name_1.startswith("2020")) #046
+
+a = "hello world"
+x = a.split(" ")
+print(x) #047
+
+ticker_4 = ticker.split("_")
+print(ticker_4) #048
+
+date = "2020-05-01"
+date_1 = date.split("-")
+print(date_1) #049
+
+data = "039490     "
+print(data.split()) #050
---
+++ python_300/051-060.py
@@ -0,0 +1,30 @@
+movie_rank = ["닥터 스트레인지", "스플릿", "럭키"] #051
+
+movie_rank.append("배트맨")
+print(movie_rank) #052
+
+movie_rank.insert(1, "슈퍼맨")
+print(movie_rank) #053
+
+movie_rank.remove("럭키")
+print(movie_rank) #054
+
+movie_rank.remove("스플릿")
+movie_rank.remove("배트맨")
+print(movie_rank) #055
+
+lang1 = ["C", "C++", "JAVA"]
+lang2 = ["Python", "Go", "C#"]
+langs = lang1 + lang2
+print(langs) #056
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+print("max :", max(nums))
+print("min :", min(nums)) #057
+
+print(sum(nums)) #058
+
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+print(len(cook)) #059
+
+print(sum(nums)/len(nums)) #060
