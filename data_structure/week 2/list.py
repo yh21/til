b9fd5b3cc93a06aaaf76a1567fccbbf533532edf
yh_21 <yh_21@naver.com>
2025-06-28 15:11:49 +0900

    .

---
+++ data_structure/week 2/list.py
@@ -0,0 +1,15 @@
+name = ['yh', 'km', 'hs', 'jh']
+print(name[2])
+
+name[3] = 'ws'
+print(name)
+
+name.append('sj')
+print(name)
+name.insert(3, 'th')
+print(name)
+
+name.remove('th')
+print(name)
+del(name[0])
+print(name)
