b389cfe3418baf81eba8bf9585ba9701f3961e0c
yh_21 <yh_21@naver.com>
2025-07-27 16:32:26 +0900

    add level_order.py file

---
+++ data_structure/week_7/level_order.py
@@ -0,0 +1,35 @@
+class BinaryClassNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+n1 = BinaryClassNode(93)
+n2 = BinaryClassNode(15)
+n3 = BinaryClassNode(19)
+n4 = BinaryClassNode(36)
+n5 = BinaryClassNode(44)
+n6 = BinaryClassNode(72)
+n7 = BinaryClassNode(83)
+
+n1.left = n2
+n1.right = n3
+n2.left = n4
+n2.right = n5
+n5.left = n6
+n5.right = n7
+
+def level_order(root_node):
+    queue= list()
+    queue.append(root_node)
+    while (True):
+        if len(queue) == 0:
+            break
+        print(queue[0].data)
+        if queue[0].left != None:
+            queue.append(queue[0].left)
+        if queue[0].right != None:
+            queue.append(queue[0].right)
+        del(queue[0])
+
+level_order(n1)
