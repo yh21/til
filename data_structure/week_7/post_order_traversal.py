0e71975db9c32e16f199bcd307c6b2d9f7ba0414
yh_21 <yh_21@naver.com>
2025-07-28 18:57:43 +0900

    add post_order_traversal.py file

---
+++ data_structure/week_7/post_order_traversal.py
@@ -0,0 +1,29 @@
+class BinaryClassNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+n1 = BinaryClassNode(93)
+n2 = BinaryClassNode(15)
+n3 = BinaryClassNode(19)
+n4 = BinaryClassNode(36)
+n5 = BinaryClassNode(44)
+n6 = BinaryClassNode(72)
+n7 = BinaryClassNode(83)
+
+n1.left = n2
+n1.right = n3
+n2.left = n4
+n2.right = n5
+n5.left = n6
+n5.right = n7
+
+def post_order(node):
+    if node.left is not None:
+        post_order(node.left)
+    if node.right is not None:
+        post_order(node.right)
+    print(node.data)
+
+post_order(n1)
