e68dbe6e5c0cd2577c4217336ba7eebab9cc3223
yh_21 <yh_21@naver.com>
2025-08-12 17:53:02 +0900

    add numberprocessing.py file

---
+++ numberprocessing.py
@@ -0,0 +1,10 @@
+print(abs(-5)) # 절댓값
+print(pow(4, 2)) # 4 ^ 2
+print(max(12 ,5))
+print(min(12, 5))
+print(round(3.14)) # 반올림 -> 3
+
+from math import *
+print(floor(3.14)) # 내림
+print(ceil(3.14)) # 올림
+print(sqrt(16)) # 제곱근
