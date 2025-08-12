561d08931786634814f849f03abccaa02a540670
yh_21 <yh_21@naver.com>
2025-08-12 16:44:21 +0900

    add avl tree, red-black tree file (no code)

---
+++ data_structure/week_9/avl_tree.py
@@ -0,0 +1,4 @@
+# avl tree
+# 1. 이진탐색 트리 조건 만족
+# 2. 임의의 Node의 Balance Factor 값이 -1, 0, 1
+
---
+++ data_structure/week_9/red-black_tree.py
@@ -0,0 +1,4 @@
+## 모든 Node 가 red / black 이며 다음을 모두 만족하는 이진 tree
+## Root Node와 NIL은 전부 black, 이때 NIL은 left, right가 none일 때, none을 대체하는 node
+## Root Node에서 임의의 Node까지의 경로에서 Red는 연속 불가
+## Root Node에서 임의의 NIL까지의 거리에서 Black의 수는 동일
