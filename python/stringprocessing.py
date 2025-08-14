00fbf6f75ac5ee67c739e711c8399a64f7f5e3c5
yh_21 <yh_21@naver.com>
2025-08-14 22:48:11 +0900

    .

---
+++ python/stringprocessing.py
@@ -0,0 +1,12 @@
+message = 'Python is nice'
+print(message.lower()) # 소문자
+print(message.upper()) # 대문자
+print(message[0].isupper()) # 대문자인지 확인
+
+print(message.replace('Python', 'Java')) # python -> java
+index = message.index('n')
+print(index) # 처음으로 n이 나오는 index
+index = message.index('n', index + 1)
+print(index) # 두 번째로 n이 나오는 index
+
+print(message.find('Java')) # 문자열이 없으면 -1 반환
