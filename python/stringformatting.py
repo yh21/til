e2a2870e6830666a148f5f33a78c99f9e901a0d9
yh_21 <yh_21@naver.com>
2025-08-15 18:03:33 +0900

    add stringformatting.py file

---
+++ python/stringformatting.py
@@ -0,0 +1,10 @@
+print('im %d years old' %20)
+print('my name is %s' % 'yh')
+
+print('im {} years old'.format(20))
+print('i like {1} and {0}'.format('red', 'blue'))
+
+age = 20
+color = 'blue'
+
+print(f'im {age} years old and i like {color} color')
